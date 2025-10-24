85333b3f195379e865f46d8fd016cd00cc46cc36
Alex Kasujja <C00315323@setu.ie>
2025-10-24 09:44:43 +0000

    update

---
+++ ping_sweep.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Script Nam: ping_sweep.sh
+#Author: Alex Kasujja
+#Date created: 24/10/2025
+#Decription: A script function that scans a small range of ip addresses in a subnet to check which ones are alive.
+
+ping_sweep()
+	subnet="172.17.0"
+	for i in {1..10}; do
+	  ip="$subnet.$i"
+	  (ping -c1 -w1 $ip &>/dev/null && echo "[+] $ip alive") &
+	done
+	wait
+}
