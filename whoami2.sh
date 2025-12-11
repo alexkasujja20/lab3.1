21211b169360f141cbf468e7095c9b86a3ac2867
Alex Kasujja <C00315323@setu.ie>
2025-12-11 00:24:28 +0000

    Add whoami2.sh script to display user info
    This script prints the username, current directory, and date.

---
+++ whoami2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Script Name: whoami.sh
+#Author: Alex Kasujja
+#Date created: 11/1/22025
+# Description: A script function that prints username, current dir and date 
+
+echo "User: $(hostname)"
+echo "Current dir: $(whoami)"
+echo "Date: $(date)"
+
+
